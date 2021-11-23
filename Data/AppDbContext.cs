d497e89e851bcb59be47b2959896d6aad93a9640
saytoaditya <saytoaditya@gmail.com>
2021-11-23 17:15:25 +0530

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using my_book.Data.Model;
+
+namespace my_book.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+   
+    
+}
---
+++ Data/Model/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace my_book.Data.Model
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsRead { get; set; }
+        public DateTime? Created { get; set; }
+        public int? Rate { get; set; }
+        public string Genre { get; set; }
+        public string Author { get; set; }
+        public string CoverUrl { get; set; }
+        public DateTime? DateAdded { get; set; }
+    }
+}
